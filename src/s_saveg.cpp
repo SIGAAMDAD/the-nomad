fb607bd97d5dd1a2ad8e93db8b2e47e06ecb22e4
SIGAAMDAD <hankburne@gmail.com>
2023-04-03 16:13:59 -0500

    updating repo

--- src/s_saveg.cpp
+++ src/s_saveg.cpp
@@ -75,16 +75,6 @@ void G_LoadGame(const char* svfile)
     version[2] = data["header"]["version.patch"];
 
     if (version[0] != _NOMAD_VERSION) {
-        bool open = true;
-        char inbuf[180];
-        LOG_WARN("version[0] != _NOMAD_VERSION, checking for user validation");
-        while (open) {
-            N_DebugWindowClear();
-            ImGui::Begin("NOTICE", &open, IMGUI_STANDARD_FLAGS);
-            ImGui::Text("This save file's version major is different from the current version, are you sure you want to continue?");
-            ImGui::InputTextWithHint();
-            ImGui::End();
-            N_DebugWindowDraw();
-        }
+        LOG_WARN("version[0] != _NOMAD_VERSION");
     }
 }
