e4323b707fb52b863233905b327c4f3818852f50
SIGAAMDAD <hankburne@gmail.com>
2024-06-20 16:32:31 -0500

    forgot the cvar

--- code/game/g_game.cpp
+++ code/game/g_game.cpp
@@ -1569,7 +1569,11 @@ void G_Init( void )
     G_InitRenderer_Cvars();
 
     G_LoadSkins();
-    
+
+    g_maxSaveSlots = Cvar_Get( "g_maxSaveSlots", "10", CVAR_LATCH | CVAR_SAVE );
+    Cvar_CheckRange( g_maxSaveSlots, "0", "1000", CVT_INT );
+    Cvar_SetDescription( g_maxSaveSlots, "Sets the maximum allowed save files to be used." );
+
     // userinfo
     Cvar_Get( "name", "The Ultimate Lad", CVAR_USERINFO | CVAR_ARCHIVE_ND );
     Cvar_Get( "skin", "raio", CVAR_USERINFO | CVAR_ARCHIVE_ND );
